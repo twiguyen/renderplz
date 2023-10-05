eec7e28e499ebf0e441d490aa813a1dc79790545
Thanh <ptnguyen1@myseneca.ca>
2023-10-05 16:33:31 -0400

    updated port

--- renderplz.js
+++ renderplz.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
 
 // Express setup
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Puppeteer browser and page objects
 let browser;
